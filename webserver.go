efb0594a82cfcd6128726fd08b4f99bb116064c0
Eli Reiners <goelyukim@gmail.com>
2020-07-14 21:00:06 -0400

    Initial commit

---
+++ webserver.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+	"io"
+  )
+  
+  func main() {
+	http.HandleFunc("/", servePage)
+	  http.ListenAndServe(":8080", nil)
+  }
+  
+  func servePage(writer http.ResponseWriter, reqest *http.Request) {
+	io.WriteString(writer, "Hello world!")
+  }
